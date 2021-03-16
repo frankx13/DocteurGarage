cba6b4ff042184df34e4cf1f78abbb83f1b3fc47
frankx13 <frankx13580@gmail.com>
2021-03-16 23:43:08 +0100

    - Finished to lay the basic UI for ServiceBoardFragment - Implemented the onclick listeners for the different sections of the UX

--- app/src/main/java/com/studio/neopanda/docteurgarage/ServiceBoardFragment.kt
+++ app/src/main/java/com/studio/neopanda/docteurgarage/ServiceBoardFragment.kt
@@ -3,15 +3,85 @@ package com.studio.neopanda.docteurgarage
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_service_board.*
+
 
 class ServiceBoardFragment : Fragment() {
+    private var countersOn = false
+    private var levelsOn = false
+    private var annotationsOn = false
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? =
         inflater.inflate(R.layout.fragment_service_board, container, false)
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        setCounterListener()
+        setLevelsListener()
+        setAnnotationsListener()
+    }
+
+    private fun setAnnotationsListener() {
+        car_counter_tv.setOnTouchListener(OnTouchListener { v, event ->
+            val DRAWABLE_LEFT = 0
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= car_counter_tv.right - car_counter_tv.compoundDrawables[DRAWABLE_LEFT].bounds.width()) {
+                    annotationsOn = !annotationsOn
+                    if (annotationsOn) {
+                        car_annotations_layout.visibility = View.VISIBLE
+                    } else {
+                        car_annotations_layout.visibility = View.GONE
+                    }
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
+    }
+
+    private fun setLevelsListener() {
+        car_counter_tv.setOnTouchListener(OnTouchListener { v, event ->
+            val DRAWABLE_LEFT = 0
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= car_counter_tv.right - car_counter_tv.compoundDrawables[DRAWABLE_LEFT].bounds.width()) {
+                    levelsOn = !levelsOn
+                    if (levelsOn) {
+                        car_levels_layout.visibility = View.VISIBLE
+                    } else {
+                        car_levels_layout.visibility = View.GONE
+                    }
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
+    }
+
+    private fun setCounterListener() {
+        car_counter_tv.setOnTouchListener(OnTouchListener { v, event ->
+            val DRAWABLE_LEFT = 0
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= car_counter_tv.right - car_counter_tv.compoundDrawables[DRAWABLE_LEFT].bounds.width()) {
+                    countersOn = !countersOn
+                    if (countersOn) {
+                        car_counter_layout.visibility = View.VISIBLE
+                    } else {
+                        car_counter_layout.visibility = View.GONE
+                    }
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
+    }
 }
